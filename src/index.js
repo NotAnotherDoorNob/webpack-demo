aee40dfc3c2d92189902d1c586a7f4c92f241a5e
NotAnotherDoorNob <lucashimesspam@gmail.com>
2023-11-02 21:13:37 -0400

    init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-import _ from 'lodash';
+import * as d3 from 'd3';
 
-
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+const square = d3.selectAll("rect");
+square.style("fill", "orange");
